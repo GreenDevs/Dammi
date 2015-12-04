02cc49e3b3f15d6c87156bf0b763c2865e9335c6
javanux <javanuxenergy@gmail.com>
2015-12-04 14:59:31 +0545

    activity details about to be done

--- app/src/main/java/com/dammi/dammi/activitydetails/DetailsActivity.java
+++ app/src/main/java/com/dammi/dammi/activitydetails/DetailsActivity.java
@@ -1,17 +1,97 @@
 package com.dammi.dammi.activitydetails;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.dammi.dammi.R;
+import com.dammi.dammi.activitydetails.fragments.AboutHostFragment;
+import com.dammi.dammi.activitydetails.fragments.AboutTourFragment;
+import com.dammi.dammi.activitydetails.fragments.ContactUsFragment;
+import com.dammi.dammi.activitydetails.fragments.RateReviewFragment;
+import com.dammi.dammi.activitydetails.fragments.ViewPagerAdapter;
 
 public class DetailsActivity extends AppCompatActivity
 {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        init();
+    }
+
+    private void init(){
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.mToolbar);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar()!=null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        viewPager=(ViewPager)findViewById(R.id.tab_viewpager);
+        if(viewPager!=null)
+        setViewPager();
+
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+        if(tabLayout!=null)
+            setTabLayout();
+    }
+
+    private void setViewPager(){
+
+        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AboutTourFragment(), "ABOUT TOUR");
+        adapter.addFragment(new AboutHostFragment(), "ABOUT HOST");
+        adapter.addFragment(new RateReviewFragment(), "REVIEWS");
+        adapter.addFragment(new ContactUsFragment(), "CONTACT US");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void setTabLayout(){
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
     }
 }
