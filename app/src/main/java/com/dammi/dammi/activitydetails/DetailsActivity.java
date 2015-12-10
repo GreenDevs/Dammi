19d0ebe2cc94912520948c2985b495de73aa6142
ishwor <echo.ishwor@gmail.com>
2015-12-10 08:41:46 +0545

    phone call intent

--- app/src/main/java/com/dammi/dammi/activitydetails/DetailsActivity.java
+++ app/src/main/java/com/dammi/dammi/activitydetails/DetailsActivity.java
@@ -1,7 +1,13 @@
 package com.dammi.dammi.activitydetails;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,65 +25,62 @@ import com.dammi.dammi.activitydetails.fragments.RateReviewFragment;
 import com.dammi.dammi.activitydetails.fragments.AboutUsFragment;
 import com.dammi.dammi.activitydetails.fragments.ViewPagerAdapter;
 
-public class DetailsActivity extends AppCompatActivity implements View.OnClickListener
-{
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
 
     private Boolean isFabOpen = false;
-    private FloatingActionButton fab,fab1,fab2;
-    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    private FloatingActionButton fab, fab1, fab2;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
     private DetailsActivity context;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        context=this;
+        context = this;
         init();
         initFab();
 
 
     }
 
-    private void init(){
+    private void init() {
 
-        Toolbar toolbar=(Toolbar)findViewById(R.id.mToolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mToolbar);
         setSupportActionBar(toolbar);
 
-        if(getSupportActionBar()!=null)
+        if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        viewPager=(ViewPager)findViewById(R.id.tab_viewpager);
-        if(viewPager!=null)
-        setViewPager();
+        viewPager = (ViewPager) findViewById(R.id.tab_viewpager);
+        if (viewPager != null)
+            setViewPager();
 
-        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
-        if(tabLayout!=null)
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        if (tabLayout != null)
             setTabLayout();
     }
 
-    private void initFab()
-    {
-        fab = (FloatingActionButton)findViewById(R.id.fab_add);
-        fab1 = (FloatingActionButton)findViewById(R.id.fab_phone);
-        fab2 = (FloatingActionButton)findViewById(R.id.fab_book);
+    private void initFab() {
+        fab = (FloatingActionButton) findViewById(R.id.fab_add);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab_phone);
+        fab2 = (FloatingActionButton) findViewById(R.id.fab_book);
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
-        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
-        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
         rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
         fab.setOnClickListener(context);
         fab1.setOnClickListener(context);
         fab2.setOnClickListener(context);
     }
 
-    private void setViewPager(){
+    private void setViewPager() {
 
-        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new ExperienceFragment(), "OVERVIEW");
         adapter.addFragment(new ExpDetailsFragment(), "DETAILS");
         adapter.addFragment(new AboutUsFragment(), "ABOUT US");
@@ -85,7 +88,7 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         viewPager.setAdapter(adapter);
     }
 
-    private void setTabLayout(){
+    private void setTabLayout() {
 
         tabLayout.setupWithViewPager(viewPager);
 
@@ -112,7 +115,7 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
                 break;
@@ -127,13 +130,13 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        switch (id){
+        switch (id) {
             case R.id.fab_add:
 
                 animateFAB();
                 break;
             case R.id.fab_phone:
-
+                call();
                 Log.d("Raj", "Fab 1");
                 break;
             case R.id.fab_book:
@@ -144,9 +147,9 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     }
 
 
-    public void animateFAB(){
+    public void animateFAB() {
 
-        if(isFabOpen){
+        if (isFabOpen) {
 
             fab.startAnimation(rotate_backward);
             fab1.startAnimation(fab_close);
@@ -164,8 +167,28 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
             fab1.setClickable(true);
             fab2.setClickable(true);
             isFabOpen = true;
-            Log.d("Raj","open");
+            Log.d("Raj", "open");
 
+        }
+    }
+
+    private void call() {
+        try {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:123456789"));
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivity(callIntent);
+        } catch (ActivityNotFoundException activityException) {
+            Log.e("helloandroid dialing example", "Call failed"+activityException);
         }
     }
 
